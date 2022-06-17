100293aef5a2f093377e9301bea9df7cf2c1d86a
McJust1988 <monoflex@rambler.ru>
2022-06-18 00:08:44 +0300

    3 commit

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -4,16 +4,16 @@ from mptt.admin import MPTTModelAdmin
 from. import models
 
 
+@admin.register(models.Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ["name", "category", "create_at", ]
-
+    list_display = ["title", "category", "author", "create_at"]
 
 @admin.register(models.News)
 class NewsAdmin(admin.ModelAdmin):
     list_display = ["name"]
 
-
 admin.site.register(models.Category, MPTTModelAdmin)
 admin.site.register(models.Tag)
-admin.site.register(models.Post, PostAdmin)
+#admin.site.register(models.Post)
+#admin.site.register(models.News)
 admin.site.register(models.Comment)
