5415994fc101043e02752a81bc8a61d20f0baf2b
McJust1988 <monoflex@rambler.ru>
2022-06-17 23:56:18 +0300

    3 commit

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -3,8 +3,17 @@ from mptt.admin import MPTTModelAdmin
 
 from. import models
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["name", "category", "create_at", ]
+
+
+@admin.register(models.News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
 admin.site.register(models.Category, MPTTModelAdmin)
 admin.site.register(models.Tag)
-admin.site.register(models.Post)
-admin.site.register(models.News)
+admin.site.register(models.Post, PostAdmin)
 admin.site.register(models.Comment)
--- mysite/news/models.py
+++ mysite/news/models.py
@@ -42,8 +42,6 @@ class Post(models.Model):
     tags = models.ManyToManyField(Tag, related_name="post")
     create_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.name
 
 class News(models.Model):
     name = models.CharField(max_length=100)
